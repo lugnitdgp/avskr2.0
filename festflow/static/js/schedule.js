34568b324d108751b55709ff05ddc24c0141fd07
rahul <rahulotwani7@gmail.com>
2018-10-07 18:28:31 +0530

    schedule updated

--- festflow/static/js/schedule.js
+++ festflow/static/js/schedule.js
@@ -1,14 +1,10 @@
-
-$("#c1").css("display","block");
-$("#c2").css("display","none");
-$("#c3").css("display","none");
-$("#c4").css("display","none");
-
-$(".parc1").addClass("parcours-active");
-
+ if($(window).width() >800){
+$(".c1").css("display","block");
+$(".c2").css("display","none");
+$(".c3").css("display","none");
+$(".c4").css("display","none");
+  $(".round1").css("background-color","white");
 $( document ).ready(function() {
-   $(".parcours-box").removeClass("parcours-active");
-
 
    $(".round1").on("click",function(){
       $(".tl-round").css("background-color","blue");
@@ -16,10 +12,10 @@ $( document ).ready(function() {
       $(".parcours-active").removeClass("parcours-active");
       $(".parc1").addClass("parcours-active");
       $(".timeline-indicator").css("width","0");
-			$("#c1").css("display","block");
-			$("#c2").css("display","none");
-			$("#c3").css("display","none");
-			$("#c4").css("display","none");
+			$(".c1").css("display","block");
+			$(".c2").css("display","none");
+			$(".c3").css("display","none");
+			$(".c4").css("display","none");
 
    })
 
@@ -30,10 +26,10 @@ $( document ).ready(function() {
       $(".parcours-active").removeClass("parcours-active");
       $(".parc2").addClass("parcours-active");
       $(".timeline-indicator").css("width","240");
-			$("#c2").css("display","block");
-			$("#c1").css("display","none");
-			$("#c3").css("display","none");
-			$("#c4").css("display","none");
+			$(".c2").css("display","block");
+			$(".c1").css("display","none");
+			$(".c3").css("display","none");
+			$(".c4").css("display","none");
 
    });
 
@@ -45,10 +41,10 @@ $( document ).ready(function() {
       $(".parcours-active").removeClass("parcours-active");
       $(".parc3").addClass("parcours-active");
       $(".timeline-indicator").css("width","480");
-			$("#c3").css("display","block");
-			$("#c2").css("display","none");
-			$("#c1").css("display","none");
-			$("#c4").css("display","none");
+			$(".c3").css("display","block");
+			$(".c2").css("display","none");
+			$(".c1").css("display","none");
+			$(".c4").css("display","none");
    })
 
    $(".round4").on("click",function(){
@@ -60,10 +56,17 @@ $( document ).ready(function() {
       $(".parcours-active").removeClass("parcours-active");
       $(".parc4").addClass("parcours-active");
       $(".timeline-indicator").css("width","720");
-			$("#c4").css("display","block");
-			$("#c2").css("display","none");
-			$("#c3").css("display","none");
-			$("#c1").css("display","none");
+			$(".c4").css("display","block");
+			$(".c2").css("display","none");
+			$(".c3").css("display","none");
+			$(".c1").css("display","none");
    })
 
 });
+}
+else  if($(window).width() <800){
+    $(".card").css("margin","5vh");
+    $(".timeline-parcours").css("display","none");
+      $(".contain").css("top","15%");
+      $(".c4").css("margin-bottom","15vh");
+}
